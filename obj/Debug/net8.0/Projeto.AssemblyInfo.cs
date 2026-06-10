e6547ed76460c502e8ab6dabac4a1ed06577f8fc
filipemedeiros2005 <160858297+al81303@alunos.utad.pt>
2026-06-10 18:09:09 +0100

    Add producer-consumer run log and update build metadata
    Add a new runtime trace log (logs/Producer-Consumer_20260610_180848.txt) capturing a producer-consumer scenario. Update generated artifacts: frontend/__pycache__/app.cpython-312.pyc changed (binary bytecode) and obj/Debug/net8.0/Projeto.AssemblyInfo.cs informational version updated along with Projeto.AssemblyInfoInputs.cache to reflect the new build identifier.

--- obj/Debug/net8.0/Projeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af07626c99ad7c339c48872f3cea5278457f9f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f8ba8ef5d044f344d6e8d9bce0c8837449ae82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
